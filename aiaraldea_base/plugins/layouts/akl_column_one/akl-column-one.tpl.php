63acf02ff28254923b38038b8c912c808dcc6322
Iñaki Ibarrola Atxa <inakiibarrola@yahoo.es>
2014-05-01 14:08:05 +0200

    Hainbat aldaketa

--- aiaraldea_base/plugins/layouts/akl_column_one/akl-column-one.tpl.php
+++ aiaraldea_base/plugins/layouts/akl_column_one/akl-column-one.tpl.php
@@ -1,7 +1,6 @@
 <?php
 // $Id: precision-column-one.tpl.php,v 1.1.2.1 2011/02/14 10:32:22 fabsor Exp $
 ?>
-akl column one
 <?php if (!empty($css_id)): ?>
   <div id="<?php print $css_id; ?>" class="clear-block">
 <?php endif; ?>
